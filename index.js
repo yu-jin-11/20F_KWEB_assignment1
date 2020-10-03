e6f0b5c86ee0e04a3223ae9d45b023958bc9469b
yu-jin-11 <rroraa@korea.ac.kr>
2020-10-03 21:22:41 +0900

    update index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http');
 
-const hostname = '127.0.0.1'; const port = 3000;
+const hostname = '127.0.0.1'; const port = 4000;
 
 const server = http.createServer((req, res) => {
         res.statusCode = 200;    
-        res.setHeader('Content-Type', 'text/plain');    
-        res.end('Hello World\n'); 
+        res.setHeader('Content-Type', 'text/plain');   
+        let date = new Date();
+        let d = date.toString(); 
+        res.end(d);
 });
 
 server.listen(port, hostname, () => {    
